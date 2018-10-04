cbf4b249afd3f0412c428b8ae8efd4bcec0fafac
Quyen <quyenta62@wru.vn>
2018-10-04 17:24:36 +0700

    fix animation scroll

--- js/action.js
+++ js/action.js
@@ -34,38 +34,7 @@ $(document).ready(function(){
         });
        
 
-        // animation on scroll
-        jQuery(function($) {
-  
-            // Function which adds the 'animated' class to any '.animatable' in view
-            var doAnimations = function() {
-              
-              // Calc current offset and get all animatables
-              var offset = $(window).scrollTop() + $(window).height(),
-                  $animatables = $('.animatable');
-              
-              // Unbind scroll handler if we have no animatables
-              if ($animatables.length == 0) {
-                $(window).off('scroll', doAnimations);
-              }
-              
-              // Check all animatables and animate them if necessary
-                  $animatables.each(function(i) {
-                 var $animatable = $(this);
-                      if (($animatable.offset().top + $animatable.height() - 300) < offset) {
-                  $animatable.removeClass('animatable').addClass('animated');
-                      }
-              });
-          
-              };
-            
-            // Hook doAnimations on scroll, and trigger a scroll
-              $(window).on('scroll', doAnimations).on("resize", function(){ // If the user resizes the window
-                $(window).height() = $(this).height(); // you'll need the new height value
-                });
-            $(window).trigger('scroll');
-          
-          });
+        
 
           $(".box-close").click(function(){
             //   $(".box-item-mm").css("display","none");
